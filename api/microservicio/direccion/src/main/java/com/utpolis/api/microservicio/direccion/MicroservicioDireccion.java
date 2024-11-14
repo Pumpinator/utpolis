10eb403e12ba35b15f09cd102550f1e3ef3d4568
Pumpinator <alejandrodcardona5@gmail.com>
2024-11-14 12:59:57 -0600

    fix rapido para que lo puedan ejecutar

--- api/microservicio/direccion/src/main/java/com/utpolis/api/microservicio/direccion/MicroservicioDireccion.java
+++ api/microservicio/direccion/src/main/java/com/utpolis/api/microservicio/direccion/MicroservicioDireccion.java
@@ -3,8 +3,10 @@ package com.utpolis.api.microservicio.direccion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient(autoRegister = false)
 @EntityScan(basePackages = {"com.utpolis.modelo.entidad"})
 public class MicroservicioDireccion {
 

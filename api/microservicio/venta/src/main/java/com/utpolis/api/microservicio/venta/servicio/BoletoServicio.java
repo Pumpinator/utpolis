9c216f1a2d843132febdaf29e0d715f533ffd407
JosueChavez1 <josuechavez0312@gmail.com>
2024-11-30 08:19:27 -0600

    Se agregaron comentarios en BoletoServicio

--- api/microservicio/venta/src/main/java/com/utpolis/api/microservicio/venta/servicio/BoletoServicio.java
+++ api/microservicio/venta/src/main/java/com/utpolis/api/microservicio/venta/servicio/BoletoServicio.java
@@ -10,22 +10,27 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
+@Service //Declaracion de Servicio
+@RequiredArgsConstructor //Genera un constructor
 public class BoletoServicio {
 
+    //Repositorios atributos de clase que interactuan con la BD
     private final AsientoRepositorio asientoRepositorio;
     private final BoletoRepositorio boletoRepositorio;
     private final FuncionRepositorio funcionRepositorio;
 
+    //Recupera los boletos de la BD y los comvierte en DTOs
     public Iterable<BoletoDto> listar() {
+        //Convierte los boletos en una lista de Boleto, convierte cada Boleto en un BoletoDto y recolecta datos en una lista
         return ((List<Boleto>) boletoRepositorio.findAll()).stream().map(this::construirDto).toList();
     }
 
+    //Obtiene un boleto por id, busca en la BD
     public BoletoDto obtener(Long id) {
         return construirDto(boletoRepositorio.findById(id).orElseThrow(() -> new RuntimeException(String.format("Boleto con id '%s' no encontrado", id))));
     }
 
+    //Construye instancia de Boleto con los datos, valida el asiento y la funcion sean existentes
     public BoletoDto crear(BoletoDto boleto) {
         return construirDto(boletoRepositorio.save(Boleto.builder()
                 .id(boleto.getId())
@@ -35,6 +40,7 @@ public class BoletoServicio {
                 .build()));
     }
 
+    //Convierte un Boleto en un DTO (Data Transfere Object)
     public BoletoDto construirDto(Boleto boleto) {
         return BoletoDto.builder()
                 .id(boleto.getId())
